03a9123987b8dc06e6837e54bf97cad843cc7359
Marshall Humble <humblejm@gmail.com>
2015-11-23 14:57:41 -0600

    re-formatted views.py

--- forms.py
+++ forms.py
@@ -1,21 +1,9 @@
 from django import forms
 from blog.models import Post
 
+
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tag', 'image', 'views']
-
-    def add_post(request):
-        context = RequestContext(request)
-        if request.method == 'POST':
-            form = PostForm(request.POST, request.FILES)
-            if form.is_valid():  # is the form valid?
-                form.save(commit=True)  # yes?
-                return redirect(index)
-            else:
-                print(form.errors)  # no? display errors to end user
-        else:
-            form = PostForm()
-        return render_to_response('blog/add_post.html', {'form': form}, context)
+        fields = ['title', 'content', 'tag', 'image']
