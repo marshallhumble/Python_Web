b6765cbdc9ce6cada4dc9d969cb0916d7978dffa
marshallhumble <humblejm@gmail.com>
2015-11-28 13:07:55 -0600

    Commit, added main app templates & icon

---
+++ main.py
@@ -0,0 +1,48 @@
+import praw
+import humanize
+from datetime import datetime
+from flask import Flask
+from flask import request, render_template
+from prawoauth2 import PrawOAuth2Mini
+
+#from token import (app_key, app_secret, access_token, refresh_token, user_agent, scopes)
+
+user_agent = 'user info by u/ummmbacon'
+scopes = ['identity', 'submit', 'read']
+app_key = 'wuZDn-3r_HFBYQ'
+app_secret = '7JQ_1pF0zn997KEDLMzR7niOJZc'
+refresh_token = '12068463-VHurYywHQ3d6yQHlDDUY6iwIuHQ'
+access_token = '12068463-PIHY08Sf_W8u5SlmhR1UduX92Zc'
+
+reddit_client = praw.Reddit(user_agent=user_agent)
+oauth_helper = PrawOAuth2Mini(reddit_client, app_key=app_key,
+                              app_secret=app_secret,
+                              access_token=access_token,
+                              refresh_token=refresh_token, scopes=scopes)
+
+app = Flask(__name__)
+
+
+def get_cake_day(username):
+    redditor = reddit_client.get_redditor(username)
+    try:
+        created_on = datetime.utcfromtimestamp(redditor.created_utc)
+    except praw.errors.NotFound:
+        return False
+    return humanize.naturalday(created_on)
+
+
+@app.route('/')
+def index():
+    error_message = 'Redditor does not exist or Shadowbanned'
+    username = request.values.get('username')
+    if not username:
+        return render_template('index.html')
+    cakeday = get_cake_day(username)
+    if cakeday:
+        return render_template('result.html', username=username,
+                               cakeday=cakeday)
+    return render_template('index.html', error_message=error_message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
