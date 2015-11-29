30f8f6c4783c9515ce74bf19d4dad4c12a2aa12f
Marshall Humble <marshallhumble@users.noreply.github.com>
2015-11-28 23:14:30 -0600

    Delete settings.py

--- settings.py
+++
@@ -1,7 +0,0 @@
-#Sample file, you will need to get an API key from Reddit
-user_agent = 'user info agent'
-scopes = ['identity', 'submit', 'read']
-app_key = '<APP_KEY>'
-app_secret = '<APP_SECRET>'
-refresh_token = '<REFRESH-TOKEN>'
-access_token = '<ACCESS-TOKEN>'
